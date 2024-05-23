d61ea4675ab61522522e8a2c60293691ee0c00ba
sseung519 <htc_desirehd@naver.com>
2024-05-23 17:16:36 +0900

    MemberController DTO 일부

--- src/main/java/com/podoarena/controller/MemberController.java
+++ src/main/java/com/podoarena/controller/MemberController.java
@@ -1,4 +1,20 @@
 package com.podoarena.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+    @GetMapping(value = "/members/login")
+    public String loginpage () {
+        return "member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/members/register")
+    public String registerPage() {
+        return "member/memberForm";
+    }
 }
--- src/main/java/com/podoarena/dto/MemberFormDto.java
+++ src/main/java/com/podoarena/dto/MemberFormDto.java
@@ -1,9 +1,17 @@
 package com.podoarena.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class MemberFormDto {
+
+    @Column(name = "member_id")
+    @NotNull
+    private Long id;
+
+    private String password;
 }
