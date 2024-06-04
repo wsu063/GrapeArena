3ba4e7abeb04dcbdfbef393d210c378aba90e01a
wsu063 <wsu063@gmail.com>
2024-06-04 15:24:18 +0900

    place

--- src/main/java/com/podoarena/controller/PlaceController.java
+++ src/main/java/com/podoarena/controller/PlaceController.java
@@ -1,17 +1,33 @@
 package com.podoarena.controller;
 
+import com.podoarena.entity.Place;
+import com.podoarena.entity.ReserveSeat;
+import com.podoarena.service.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class PlaceController {
 
-    //공연장 목록
-    @GetMapping(value = "/admin/placeList")
-    public String placeList() {
+    private final PlaceService placeService;
 
+    //공연장 목록
+    @GetMapping(value = {"/admin/places/list", "/admin/places/list/{page}"})
+    public String placeList(Model model) {
+
+        List<Place> placeList = placeService.getPlaceList();
+
+        model.addAttribute("placeList", placeList);
 
         return "admin/placeList";
     }
---
+++ src/main/java/com/podoarena/repository/PlaceRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.podoarena.repository;
+
+import com.podoarena.entity.Place;
+
+import java.util.List;
+
+public interface PlaceRepositoryCustom {
+    List<Place> getPlaceAll();
+}
