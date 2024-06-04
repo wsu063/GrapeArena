63b45dd86aec917557a6cc170b5e3d5fef35affb
Jjeongmean <exo1461@naver.com>
2024-06-04 10:30:37 +0900

    CartService update

--- src/main/java/com/podoarena/repository/GoodsCartRepository.java
+++ src/main/java/com/podoarena/repository/GoodsCartRepository.java
@@ -1,9 +1,15 @@
 package com.podoarena.repository;
 
+import com.podoarena.dto.GoodsCartDto;
 import com.podoarena.entity.Cart;
 import com.podoarena.entity.GoodsCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GoodsCartRepository extends JpaRepository<GoodsCart, Long> {
     GoodsCart findByGoodsId(Long id);
+
+    List<GoodsCartDto> findGoodsCartDtoList (@Param("goodscartId") Long goodscartId);
 }
