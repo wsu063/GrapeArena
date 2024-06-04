6f8238f8bf810b5ce5df0d2ce7123e1653d94d36
wsu063 <wsu063@gmail.com>
2024-06-04 09:38:18 +0900

    엔티티 수정

---
+++ src/main/java/com/podoarena/entity/PlaceConcert.java
@@ -0,0 +1,34 @@
+package com.podoarena.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "place_concert")
+@Getter
+@Setter
+@ToString
+public class PlaceConcert {
+    @Id
+    @Column(name = "place_concert_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "concert_id")
+    private Concert concert;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    public static PlaceConcert createPlaceConcert(Place place, Concert concert) {
+        PlaceConcert placeConcert = new PlaceConcert();
+        placeConcert.setConcert(concert);
+        placeConcert.setPlace(place);
+
+        return placeConcert;
+    }
+}
