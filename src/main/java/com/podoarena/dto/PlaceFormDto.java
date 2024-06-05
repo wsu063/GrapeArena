655715abadabfb267fd959124a572f5033b4669b
wsu063 <wsu063@gmail.com>
2024-06-05 10:11:36 +0900

    공연장 완성

--- src/main/java/com/podoarena/dto/PlaceFormDto.java
+++ src/main/java/com/podoarena/dto/PlaceFormDto.java
@@ -3,6 +3,7 @@ package com.podoarena.dto;
 import com.podoarena.constant.ConcertState;
 import com.podoarena.entity.Concert;
 import com.podoarena.entity.Place;
+import com.podoarena.entity.PlaceImg;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,7 +32,19 @@ public class PlaceFormDto {
 
     //dto -> entity
     public Place createPlace() {
-        return modelMapper.map(this, Place.class);
+        Place place = new Place();
+        place.setId(this.id);
+        place.setPlaceName(this.placeName);
+        place.setPlaceBatch(this.placeBatch);
+        place.setPlaceLocation(this.placeLocation);
+        if (this.placeImgDto != null) {
+            PlaceImg placeImg = new PlaceImg();
+            placeImg.setImgUrl(this.placeImgDto.getImgUrl());
+            placeImg.setImgName(this.placeImgDto.getImgName());
+            place.setPlaceImg(placeImg);
+        }
+
+        return place;
     }
     //entity -> dto
     public static PlaceFormDto of(Place place) {
