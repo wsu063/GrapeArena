cf9c8613952923fe0a162ab680b7364247621ea8
wsu063 <wsu063@gmail.com>
2024-06-06 19:58:03 +0900

    콘서트 목록(관리자) 삭제 구현

--- src/main/java/com/podoarena/controller/ConcertController.java
+++ src/main/java/com/podoarena/controller/ConcertController.java
@@ -12,18 +12,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -94,6 +94,14 @@ public class ConcertController {
             return "admin/concertModifyForm";
         }
         return "admin/concertModifyForm";
+    }
+
+    @DeleteMapping(value = "/admin/concerts/delete/{concertId}")
+    private @ResponseBody ResponseEntity concertDelete(@PathVariable("concertId") Long concertId) {
+
+        concertService.deleteConcert(concertId);
+
+        return new ResponseEntity<Long>(concertId, HttpStatus.OK);
     }
 
     //콘서트 목록 (관리자)
