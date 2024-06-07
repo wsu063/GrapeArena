acb3457363c4b51d7512b7b4a4fe2ace6fb8296b
Jjeongmean <exo1461@naver.com>
2024-06-07 11:59:23 +0900

    order update

--- src/main/java/com/podoarena/dto/OrderHistDto.java
+++ src/main/java/com/podoarena/dto/OrderHistDto.java
@@ -6,6 +6,7 @@ import com.podoarena.entity.Orders;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +17,17 @@ public class OrderHistDto {
     // entity -> Dto로 변환
     public OrderHistDto(Orders orders) {
         this.orderId = orders.getId();
-
+        this.orderDate = orders.getOrderDate()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = orders.getOrderStatus();
     }
 
     private Long orderId;
     private String orderDate;
     private OrderStatus orderStatus;
-    private List<GoodsCartDto> goodsCartDtoList = new ArrayList<>();
+    private List<GoodsCartDto> goodsCartDtoList = new ArrayList<>(); //주문상태리스트
+
+    //goodsCartDto 객체를 주문 상품 리스트에 추가
+    public void addGoodsCartDto(GoodsCartDto goodsCartDto)  {this.goodsCartDtoList.add(goodsCartDto);}
+
 }
