a8d539b37aca3208da2d0312df564dc16640bfb1
Jjeongmean <exo1461@naver.com>
2024-06-12 11:09:38 +0900

    update

--- src/main/java/com/podoarena/controller/CartController.java
+++ src/main/java/com/podoarena/controller/CartController.java
@@ -22,6 +22,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,16 +33,6 @@ public class CartController {
     private final MemberService memberService;
     private final CartRepository cartRepository;
     private final GoodsService goodsService;
-
-
-
-//    //회원의 장바구니 목록을 조회
-//    @GetMapping(value = "/members/cart")
-//    public String orderHist(Principal principal, Model model) {
-//        List<GoodsCartDto> goodsCartDtoList = cartService.getGoodsCartDtoList(principal.getName());
-//        model.addAttribute("goodsCarts", goodsCartDtoList); //model에 카트 목록 추가
-//        return "members/cart";
-//    }
 
 
     @GetMapping(value = "/members/cart")
@@ -121,51 +112,28 @@ public class CartController {
     }
 
     //카트에 담긴 굿즈 주문
-//    @PostMapping(value = "/cart/orders")
-//    public @ResponseBody ResponseEntity orderGoodsCart(@RequestBody OrderDto orderDto, Principal principal) {
-//        List<OrderDto> orderDtoList = orderDto.
-//    }
+    @PostMapping(value = "/cart/orders")
+    public @ResponseBody ResponseEntity orderGoodsCart(@RequestBody OrderDto orderDto, Principal principal) {
+        List<OrderDto> orderDtoList = orderDto.getOrderDtoList();
+
+        if (orderDtoList == null || orderDtoList.size() == 0) {
+            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.FORBIDDEN);
+        }
+
+        for (OrderDto orderDtos : orderDtoList ) {
+            if (!cartService.validateGoodsCart(orderDtos.getGoodsId(), principal.getName())){
+                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+            }
+        }
+
+        Long ordersId = cartService.orderGoodsCart(new ArrayList<>(), principal.getName());
+        return new ResponseEntity<Long>(ordersId, HttpStatus.OK);
+    }
+
 
 
 }
 
-
-
-
-//    @PostMapping(value = "/members/cart")
-//    public @ResponseBody ResponseEntity order(@RequestBody @Valid GoodsFormDto goodsFormDto, BindingResult bindingResult, Principal principal) {
-//        //유효성 검증에서 오류있는 경우
-//        if (bindingResult.hasErrors()) {
-//            StringBuilder sb = new StringBuilder();
-//            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-//
-//            //오류 메세지들을 StringBuilder에 추가
-//            for (FieldError fieldError : fieldErrors) {
-//                sb.append(fieldError.getDefaultMessage());
-//            }
-//
-//            //BAD_REQUEST 상태 코드와 함께 오류 메시지 반환
-//            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST); // 에러 메시지와 함께 400 상태 코드 반환
-//        }
-//
-//        GoodsCartDto goodsCartDto = new GoodsCartDto();
-//        goodsCartDto.createGoodsCart();
-//
-//        //현재 사용자 이메일 가져오기
-//        String email = principal.getName();
-//        Long goodsCartId;
-//
-//        try {
-//            //카트에 항목 추가
-//            goodsCartId = cartService.addCart(goodsCartDto, memberRepository.findByEmail(email));
-//        } catch (Exception e) {
-//            // 예외 발생 시 에러 메시지와 함께 400 상태 코드 반환
-//            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
-//        }
-//
-//        // 성공 시 굿즈ID와 함께 200 상태 코드 반환
-//        return new ResponseEntity<Long>(goodsCartId, HttpStatus.OK);
-//    }
 
 
 
--- src/main/java/com/podoarena/dto/OrderDto.java
+++ src/main/java/com/podoarena/dto/OrderDto.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Getter
 @Setter
@@ -17,4 +19,6 @@ public class OrderDto {
     @Min(value = 1, message = "최소 주문수량은 1개 입니다.")
     @Max(value = 999, message = "최대 주문수량은 999개 입니다.")
     private int count;
+
+    private List<OrderDto> orderDtoList;
 }
